7f5febe9482c1b57108e2c9ed1f94a1225558748
ANACONDAJUTS <59391646+ANACONDAJUTS95@users.noreply.github.com>
2024-08-28 16:30:57 +0800

    Fixed the javascript file now and it's working as intended ✨

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
 function play(str) {
-    const display = document.getElementById('display');
-  
-    if (str === "Q") {
-      display.innerText = "Heater 1";
-    } else if (str === "W") {
-      display.innerText = "Heater 2";
-    } else if (str === "E") {
-      display.innerText = "Heater 3";
-    } else if (str === "A") {
-      display.innerText = "Heater 4";
-    } else if (str === "S") {
-      display.innerText = "Clap";
-    } else if (str === "D") {
-      display.innerText = "Open-HH";
-    } else if (str === "Z") {
-      display.innerText = "Kick-n'-Hat";
-    } else if (str === "X") {
-      display.innerText = "Kick";
-    } else if (str === "C") {
-      display.innerText = "Closed-HH";
-    }
-    
-    const audio = document.getElementById(str);
-    audio.currentTime = 0;
-    audio.play();
+  const display = document.getElementById('display');
+
+  // Display the sound name
+  const sounds = {
+    "Q": "Heater 1",
+    "W": "Heater 2",
+    "E": "Heater 3",
+    "A": "Heater 4",
+    "S": "Clap",
+    "D": "Open-HH",
+    "Z": "Kick-n'-Hat",
+    "X": "Kick",
+    "C": "Closed-HH"
+  };
+  display.innerText = sounds[str];
+
+  // Play the sound
+  const audio = document.getElementById(str);
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function addActiveClass(key) {
+  const pad = document.getElementById(key);
+  pad.classList.add('drum-pad-active');
+  setTimeout(() => pad.classList.remove('drum-pad-active'), 100);
+}
+
+window.document.onkeyup = function(event) {
+  const keystroke = event.key.toUpperCase();
+  if (["Q", "W", "E", "A", "S", "D", "Z", "X", "C"].includes(keystroke)) {
+    play(keystroke);
+    addActiveClass(keystroke);
   }
-  
-  function addActiveClass(key) {
-    const pad = document.querySelector(`#${key.toUpperCase()}`).parentElement;
-    pad.classList.add('drum-pad-active');
-    setTimeout(() => pad.classList.remove('drum-pad-active'), 100);
-  }
-  
-  window.document.onkeyup = function(event) {
-    const keystroke = event.key.toUpperCase();
-    if (["Q", "W", "E", "A", "S", "D", "Z", "X", "C"].includes(keystroke)) {
-      play(keystroke);
-      addActiveClass(keystroke);
-    }
-  }
-  
+}
